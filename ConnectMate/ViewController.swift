8857c1145902ba8cddf8be247d2bf86162ddfd8a
VanJay <vanjay.dev@gmail.com>
2026-03-26 23:44:12 +0800

    初始化工程

---
+++ ConnectMate/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ConnectMate
+//
+//  Created by VanJay on 2026/3/26.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ ConnectMateTests/ConnectMateTests.swift
@@ -0,0 +1,19 @@
+//
+//  ConnectMateTests.swift
+//  ConnectMateTests
+//
+//  Created by VanJay on 2026/3/26.
+//
+
+import Testing
+@testable import ConnectMate
+
+struct ConnectMateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
